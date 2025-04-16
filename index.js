c5b333a36bb345d03b7904bbb5251810e898f5ed
vaibhav-panday <vaibhavpanday1234@gmail.com>
2025-04-16 19:18:41 +0530

    second commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
-
+require("dotenv").config()
 const Blog = require('./models/blog');
 
 const userRoute = require('./routes/user');
@@ -11,8 +11,8 @@ const blogRoute = require('./routes/blog');
 const { checkForAuthenticationCookie } = require("./middlewares/authentication");
 
 const app = express()
-const Port = 8002; 
-mongoose.connect('mongodb://localhost:27017/blogify').then(e => console.log("mongodb connected"));
+const Port = process.env.PORT || 8002; 
+mongoose.connect(process.env.mongoURL).then(e => console.log("mongodb connected"));
 
 app.set('view engine','ejs')
 app.set('views', path.resolve("./views"));
